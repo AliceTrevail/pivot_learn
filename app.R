0da5add099c3970111502c5180ec29e65305f34a
Alice Trevail <a.trevail@exeter.ac.uk>
2023-11-13 18:05:57 +0000

    finish times example

--- app.R
+++ app.R
@@ -8,7 +8,7 @@
 #
 
 
-pacman::p_load(tidyverse, shiny, shinydashboard, DT, palmerpenguins)
+pacman::p_load(tidyverse, shiny, shinydashboard, DT, palmerpenguins, lubridate)
 
 
 
@@ -34,9 +34,35 @@ ui <- navbarPage("Microteach: Learn some data manipulation!",
                          mainPanel(
                              tableOutput("quadrat_Table"),
                              verbatimTextOutput("quadrat_code"),
-                             tableOutput("quadrat_pivoted")
+                             tableOutput("quadrat_pivoted"),
+                             plotOutput("quadrat_plot", width = "60%", height = "200px")
                          )
                      )),
+                 
+                 
+                 tabPanel(
+                     "Finish Times",
+                     
+                     # Sidebar with a slider input for number of bins 
+                     sidebarLayout(
+                         sidebarPanel(
+                             p("Choose new column names"),
+                             actionButton("times_go", "then Click here to pivot!"),
+                             p(""),
+                             p("Enter name for new column that will contain old column names"),
+                             textInput("times_names", "names_to = ", ""),
+                             p("Enter name for new column that will contain values"),
+                             textInput("times_values", "values_to = ", "")
+                         ),
+                         
+                         # Show a plot of the generated distribution
+                         mainPanel(
+                             tableOutput("times_Table"),
+                             verbatimTextOutput("times_code"),
+                             tableOutput("times_pivoted")
+                         )
+                     )),
+                 
                  
                  tabPanel(
                    "Penguins",
@@ -95,9 +121,64 @@ server <- function(input, output) {
                          values_to = paste0(input$quadrat_values))
     }, ignoreNULL = FALSE)
     
+    
+    quadrat_pivot_plot <- eventReactive(input$quadrat_go, {
+        req(input$quadrat_names)
+        quadrat_data %>%
+            pivot_longer(.,
+                         cols = SpeciesA:SpeciesC,
+                         names_to = paste0(input$quadrat_names),
+                         values_to = paste0(input$quadrat_values)) %>%
+            ggplot(., 
+                   aes(x = get(paste0(input$quadrat_names)),
+                       y = get(paste0(input$quadrat_values)),
+                       fill = Quadrat))+
+            geom_col(position = "dodge")+
+            labs(x = paste0(input$quadrat_names), y = paste0(input$quadrat_values))+
+            theme_light()
+    }, ignoreNULL = FALSE)
+    
     output$quadrat_pivoted <- renderTable({
         quadrat_data_pivot()
     })
+    
+    output$quadrat_plot <- renderPlot({
+        quadrat_pivot_plot()
+    })
+    
+    #### Times #####
+    times_data <- tibble(
+        Athlete = c("Alice", "Olli"),
+        run1 = c("28:52", "22:39"),
+        run2 = c("15:29", "22:25"),
+        run3 = c("27:10", "20:56")
+    )
+    
+    output$times_Table <- renderTable({
+        times_data
+    })
+    
+    output$times_code <- renderPrint({
+        cat("pivot_longer(data,", 
+            "\n\tcols = run1:run3",
+            "\n\tnames_to = ", input$times_names,
+            "\n\tvalues_to = ", input$times_values, " )")
+    })
+    
+    times_data_pivot <- eventReactive(input$times_go, {
+        req(input$times_names)
+        times_data %>%
+            pivot_longer(.,
+                         cols = run1:run3,
+                         names_to = paste0(input$times_names),
+                         values_to = paste0(input$times_values))
+    }, ignoreNULL = FALSE)
+    
+    output$times_pivoted <- renderTable({
+        times_data_pivot()
+    })
+    
+    
     
     #### Penguins #####
     output$penguin_Table <- renderTable({
